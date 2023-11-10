ff7992622ee8d84395eafff96a497eee5d588de6
Gabriel <rev3npvp@gmail.com>
2023-11-10 11:52:52 -0300

    AdicaoDeVagas

--- src/routes/Projects/Vagas/Vagas.tsx
+++ src/routes/Projects/Vagas/Vagas.tsx
@@ -46,7 +46,7 @@ export function Vagas() {
       link: "https://drestagio.gupy.io/jobs/5355782?jobBoardSource=gupy_public_page",
     },
     {
-      id: 1,
+      id: 2,
       titulo: "VAGA AUXILIAR ADMINISTRATIVO",
       cidade: " FORTALEZA",
       tipoContrato: "ESTÁGIO",
@@ -54,15 +54,15 @@ export function Vagas() {
       link: "https://drestagio.gupy.io/",
     },
     {
-      id: 1,
+      id: 3,
       titulo: "VAGA AUXILIAR DE COMÉRCIO E VAREJO",
       cidade: "JÓQUEI CLUBE , FORTALEZA",
       tipoContrato: "ESTÁGIO",
       descricao: "Descrição da vaga de vendedor.",
-      link: "https://drestagio.gupy.io/jobs/5630411?jobBoardSource=gupy_public_page",
+      link: "https://drestagio.gupy.io/jobs/5121028?jobBoardSource=gupy_public_page",
     },
     {
-      id: 2,
+      id: 4,
       titulo: "VAGA AUXILIAR DE COMÉRCIO E VAREJO",
       cidade: "RODOLFO TEÓFILO, FORTALEZA",
       tipoContrato: "ESTÁGIO",
@@ -70,7 +70,7 @@ export function Vagas() {
       link: "https://drestagio.gupy.io/jobs/5508610?jobBoardSource=gupy_public_page",
     },
     {
-      id: 2,
+      id: 5,
       titulo: "VAGA AUXILIAR ADMINISTRATIVO",
       cidade: "JABUTI, EUSÉBIO",
       tipoContrato: "ESTÁGIO",
@@ -78,7 +78,7 @@ export function Vagas() {
       link: "https://drestagio.gupy.io/jobs/5358038?jobBoardSource=gupy_public_page",
     },
     {
-      id: 2,
+      id: 6,
       titulo: "VAGA AUXILIAR DE COMÉRCIO E VAREJO",
       cidade: "MORADA NOVA",
       tipoContrato: "ESTÁGIO",
@@ -86,7 +86,7 @@ export function Vagas() {
       link: "https://drestagio.gupy.io/jobs/5538298?jobBoardSource=gupy_public_page",
     },
     {
-      id: 2,
+      id: 7,
       titulo: "VAGA AUXILIAR DE COMÉRCIO E VAREJO",
       cidade: "EDSON QUEIROZ, FORTALEZA",
       tipoContrato: "ESTÁGIO",
@@ -94,12 +94,100 @@ export function Vagas() {
       link: "https://drestagio.gupy.io/jobs/5139889?jobBoardSource=gupy_public_page",
     },
     {
-      id: 2,
-      titulo: "VENDEDOR EXTERNO",
-      cidade: "TEIXEIRA DE FREITAS, BAHIA",
-      tipoContrato: "CLT",
+      id: 8,
+      titulo: "VAGA AUXILIAR DE COMÉRCIO E VAREJO",
+      cidade: "CURIÓ / LAGOA REDONDA , FORTALEZA",
+      tipoContrato: "ESTÁGIO",
       descricao: "", // ou null, se não houver descrição
-      link: "https://drestagio.gupy.io/",
+      link: "https://drestagio.gupy.io/jobs/5722709?jobBoardSource=gupy_public_page",
+    },
+    {
+      id: 9,
+      titulo: "VAGA AUXILIAR DE COMÉRCIO E VAREJO",
+      cidade: " AMONTADA",
+      tipoContrato: "ESTÁGIO",
+      descricao: "", // ou null, se não houver descrição
+      link: "https://drestagio.gupy.io/jobs/5378461?jobBoardSource=gupy_public_page",
+    },
+    {
+      id: 10,
+      titulo: "VAGA AUXILIAR DE COMÉRCIO E VAREJO",
+      cidade: "JOSÉ WALTER , FORTALEZA",
+      tipoContrato: "ESTÁGIO",
+      descricao: "", // ou null, se não houver descrição
+      link: "https://drestagio.gupy.io/jobs/5355753?jobBoardSource=gupy_public_page",
+    },
+    {
+      id: 11,
+      titulo: "VAGA AUXILIAR DE COMÉRCIO E VAREJO",
+      cidade: "JOÃO XXIII , FORTALEZA",
+      tipoContrato: "ESTÁGIO",
+      descricao: "", // ou null, se não houver descrição
+      link: "https://drestagio.gupy.io/jobs/5355998?jobBoardSource=gupy_public_page",
+    },
+    {
+      id: 12,
+      titulo: "VAGA AUXILIAR DE COMÉRCIO E VAREJO",
+      cidade: "QUINTINO CUNHA , FORTALEZA",
+      tipoContrato: "ESTÁGIO",
+      descricao: "", // ou null, se não houver descrição
+      link: "https://drestagio.gupy.io/jobs/5355647?jobBoardSource=gupy_public_page",
+    },
+    {
+      id: 13,
+      titulo: "VAGA MARKETING",
+      cidade: "JACARECANGA , FORTALEZA",
+      tipoContrato: "ESTÁGIO SUPERIOR",
+      descricao: "", // ou null, se não houver descrição
+      link: "https://drestagio.gupy.io/jobs/5358001?jobBoardSource=gupy_public_page",
+    },
+    {
+      id: 14,
+      titulo: "VAGA SEGURANÇA DO TRABALHO ",
+      cidade: "JABUTI , EUSÉBIO",
+      tipoContrato: "ESTÁGIO",
+      descricao: "", // ou null, se não houver descrição
+      link: "https://drestagio.gupy.io/jobs/5355693?jobBoardSource=gupy_public_page",
+    },
+    {
+      id: 15,
+      titulo: "VAGA MARKETING",
+      cidade: "RUSSAS - CE",
+      tipoContrato: "ESTÁGIO SUPERIOR",
+      descricao: "", // ou null, se não houver descrição
+      link: "https://drestagio.gupy.io/jobs/5672484?jobBoardSource=gupy_public_page",
+    },
+    {
+      id: 16,
+      titulo: "VAGA AUXILIAR DE COMÉRCIO E VAREJO",
+      cidade: "MOSSORÓ , RIO GRANDE DO NORTE",
+      tipoContrato: "ESTÁGIO",
+      descricao: "", // ou null, se não houver descrição
+      link: "https://drestagio.gupy.io/jobs/5126307?jobBoardSource=gupy_public_page",
+    },
+    {
+      id: 17,
+      titulo: "VAGA AUXILIAR DE COMÉRCIO E VAREJO",
+      cidade: "MARACANAÚ",
+      tipoContrato: "ESTÁGIO",
+      descricao: "", // ou null, se não houver descrição
+      link: "https://drestagio.gupy.io/jobs/5126984?jobBoardSource=gupy_public_page",
+    },
+    {
+      id: 18,
+      titulo: "VAGA AUXILIAR DE COMÉRCIO E VAREJO",
+      cidade: "ITAPIPOCA",
+      tipoContrato: "ESTÁGIO",
+      descricao: "", // ou null, se não houver descrição
+      link: "https://drestagio.gupy.io/jobs/5309551?jobBoardSource=gupy_public_page",
+    },
+    {
+      id: 19,
+      titulo: "VAGA AUXILIAR DE COMÉRCIO E VAREJO",
+      cidade: "NATAL, RIO GRANDE DO NORTE",
+      tipoContrato: "ESTÁGIO",
+      descricao: "", // ou null, se não houver descrição
+      link: "https://drestagio.gupy.io/jobs/5131899?jobBoardSource=gupy_public_page",
     },
   ]);
   useEffect(() => {
@@ -213,7 +301,10 @@ export function Vagas() {
           <div className="iODkpvDR">
             <div className="styles__ContainerEmpty-sc-19uu72c-6DR euoTqgDR">
               <h2>Ops!</h2>
-              <h4>Ainda não existe nenhuma vaga cadastrada aqui.</h4>
+              <h4>
+                Ainda não existe nenhuma vaga cadastrada para a sua cidade ou
+                bairro aqui.
+              </h4>
               <p>
                 Enquanto isso, você pode enviar seu currículo para nosso banco
                 de talentos, assim nossos recrutadores poderão encontrar a vaga
