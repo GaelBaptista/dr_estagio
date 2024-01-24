1fc7fea275d06754e274ab6079298d815caad5dc
Gabriel <rev3npvp@gmail.com>
2024-01-24 10:13:55 -0300

    subida_site_error_remove

--- src/routes/Solucoes_RH/Pales_Trein/pales_trein.tsx
+++ src/routes/Solucoes_RH/Pales_Trein/pales_trein.tsx
@@ -1,13 +1,4 @@
 
-import home_pn1 from './assets_pales_trein/hero-banner-1.jpg'
-import home_pn2 from './assets_pales_trein/hero-banner-2.jpg'
-import home_svg from './assets_pales_trein/hero-bg.svg'
-import home_shape from './assets_pales_trein/hero-shape-2.png'
-import about from './assets_pales_trein/about-banner.jpg'
-import shapeAbout from './assets_pales_trein/Fale conosco.svg'
-import aboutshape4 from  './assets_pales_trein/about-shape-4.svg'
-import aboutshape3 from  './assets_pales_trein/about-shape-3.jpg'
-import aboutshape1 from './assets_pales_trein/about-shape-1.svg'
 
 
 
