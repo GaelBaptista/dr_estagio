b06dc5087b362820229ea04afc592326e080bcad
Gabriel <rev3npvp@gmail.com>
2024-08-07 11:09:49 -0300

    correcao de eros ortograficos

--- src/routes/Blog/PagesBlog/Fak/Fak.tsx
+++ src/routes/Blog/PagesBlog/Fak/Fak.tsx
@@ -1,85 +1,49 @@
-import './Fak.css'
+import "./Fak.css";
 
-
-
-export function   Fak() {
-    return (
-    <div className='blog-article-container'>
-      <header className='header__blog_guia'>
+export function Fak() {
+  return (
+    <div className="blog-article-container">
+      <header className="header__blog_guia">
         <div className="header-banner_blog">
           <h1>Principais Dúvidas</h1>
         </div>
         <div className="clear_blog"></div>
-        
       </header>
 
       <section className="content_blog_article">
-        <article className='article_blog'>
-         
-          
-          
+        <article className="article_blog">
           <h1>O estágio é regulamentado? </h1>
           <br />
-          
-          <p>Sim. O estágio é regulamentado pela Lei nº 11.788, de 25 de setembro de 2008, 
-conhecida como "Lei do Estágio". Essa lei estabelece as diretrizes para a realização de 
-estágios no país, visando proporcionar aos estudantes uma experiência prática que 
-complemente sua formação acadêmica. </p>
+
+          <p>
+            Sim. O estágio é regulamentado pela Lei nº 11.788, de 25 de setembro
+            de 2008, conhecida como "Lei do Estágio". Essa lei estabelece as
+            diretrizes para a realização de estágios no país, visando
+            proporcionar aos estudantes uma experiência prática que complemente
+            sua formação acadêmica.{" "}
+          </p>
           <br />
-            <h1>Quem pode ser estagiário?  </h1>
+          <h1>Quem pode ser estagiário? </h1>
           <br />
-          
-          <p>De acordo com a legislação brasileira, estão aptos a realizar estágio os estudantes a 
-partir de 16 anos que estejam devidamente matriculados no ensino profissionalizante, 
-médio, educação especial, superior e nos anos finais do fundamental – na modalidade 
-profissional da educação de jovens e adultos (EJA). Contudo para o setor varejista 
-orientamos jovens a partir de 18 anos.</p>
+
+          <p>
+            De acordo com a legislação brasileira, estão aptos a realizar
+            estágio os estudantes a partir de 16 anos que estejam devidamente
+            matriculados no ensino profissionalizante, médio, educação especial,
+            superior e nos anos finais do fundamental – na modalidade
+            profissional da educação de jovens e adultos (EJA). Contudo para o
+            setor varejista orientamos jovens a partir de 18 anos.
+          </p>
           <br />
-          <h1>Quem pode contratar estagiários?  </h1>
+          <h1>Quem pode contratar estagiários? </h1>
           <br />
-          
-          <p>Empresas, órgãos públicos (federal, estadual e municipal), autarquias, fundações e até 
-profissionais liberais de nível superior, devidamente registrados em seus conselhos 
-profissionais.
-</p>
-            <br />
-          <h1>4. Gerencie Seu Tempo</h1>
+
+          <p>
+            Empresas, órgãos públicos (federal, estadual e municipal),
+            autarquias, fundações e até profissionais liberais de nível
+            superior, devidamente registrados em seus conselhos profissionais.
+          </p>
           <br />
-          
-          <p>Aprenda a priorizar tarefas e gerenciar seu tempo de forma eficaz. Utilize ferramentas como listas de afazeres e calendários para manter-se organizado. Cumprir prazos é crucial no mundo profissional.</p>
-          <br />
-          <h1>5. Seja Adaptável e Flexível</h1>
-          <br />
-          
-          <p>O ambiente de trabalho está sempre mudando. Esteja preparado para se adaptar a novas situações, aprender novas habilidades e enfrentar desafios de maneira positiva. A flexibilidade é uma qualidade valiosa. </p>
-          <br />
-          <h1>6. Vista-se Adequadamente e Mantenha uma Postura Profissional</h1>
-          <br />
-          
-          <p>A aparência e a postura profissional são importantes. Vista-se de forma adequada ao código de vestimenta da empresa e mantenha uma postura confiante e respeitosa. </p>
-          <br />
-          <h1>7. Aprenda com os Erros</h1>
-          <br />
-          
-          <p>Ninguém é perfeito. Se cometer um erro, veja-o como uma oportunidade de aprendizado. Assuma a responsabilidade, aprenda com a experiência e evite repetir os mesmos erros no futuro. </p>
-          <br />
-          <h1>8. Peça Ajuda Quando Necessário</h1>
-          <br />
-          
-          <p>Não tenha medo de pedir ajuda se estiver inseguro sobre uma tarefa. É melhor esclarecer dúvidas do que fazer algo errado por falta de compreensão. Seus colegas de trabalho e supervisores estão lá para apoiá-lo.</p>
-          <br />
-          <h1>9. Demonstre Gratidão e Apreço</h1>
-          <br />
-          
-          <p>Mostre gratidão pelas oportunidades e pelo aprendizado que você está recebendo durante o estágio. Um simples "obrigado" pode fazer uma grande diferença e criar um ambiente de trabalho positivo.</p>
-          <br />
-          
-      
-          
-          <p>Lembre-se, seu estágio é mais do que apenas uma posição temporária. É uma chance de aprender, crescer e construir as bases para sua carreira futura. Ao ser proativo, comunicativo, flexível e grato, você não apenas terá um estágio bem-sucedido, mas também estará se preparando para um futuro profissional brilhante. Boa sorte!</p>
-          
-          
-          
         </article>
         {/* <aside className='aside_blog'>
           <img src={logoGupy} alt="Placeholder"/>
--- src/routes/ProgramaDeEstagio/Estagio/PrEst.tsx
+++ src/routes/ProgramaDeEstagio/Estagio/PrEst.tsx
@@ -1,194 +1,196 @@
-
 import { useState } from "react";
 import "./PrEst.css";
 
-import {FAQ} from "./FAQ";
-import { FAQItem } from './FAQ';
+import { FAQ } from "./FAQ";
+import { FAQItem } from "./FAQ";
 import CarouselComponent from "../servicos_pr/Cardservico";
 import { Testimonials_ } from "../servicos_pr/testimonials";
 import { Varejo_Farmacia } from "../servicos_pr/Varejo_farma";
 
+export function PrEst() {
+  const categories = [
+    "Benefícios Programa de Estágio",
+    "Cota de Estagiário",
+    "Benefícios Sociais",
+    "Benefícios Trabalhistas",
+    "Como Aderir ao programa de estágio",
+    // adicionar mais se precisar
+  ];
 
+  const initialCategoryFaqs: { [key: string]: FAQItem[] } = {
+    "Benefícios Programa de Estágio": [
+      {
+        question: "Custo reduzido",
+        answer:
+          "O estágio é regido por Legislação própria e, observados os requisitos legais, não estabelece vínculo empregatício de qualquer natureza, assim ficam isentos de alguns impostos trabalhistas, tais como: FGTS, INSS, 13º salário, ⅓ sobre férias. Isso só é possível, pois não se trata de um posto com registro na CTPS (Carteira de Trabalho e Previdência Social). Isso ajuda a reduzir os custos e possibilita as empresas investir em talentos promissores.",
+        open: false,
+      },
+      {
+        question: "Prospeção e renovação de talento",
+        answer:
+          "Contratar estagiários permite que a empresa atraia talentos, trazendo novas ideias, perspectivas e habilidades para a sua equipe possibilitando a identificação dos mais talentosos e adequados para a cultura organizacional, permitindo que a empresa avalie potenciais futuros colaboradores.",
+        open: false,
+      },
+      {
+        question: "Treinamentos e desenvolvimentos",
+        answer:
+          "Os estagiários são excelentes candidatos para treinamento e desenvolvimento, pois ainda estão em fase de aprendizado e podem ser moldados de acordo com as necessidades da empresa.",
+        open: false,
+      },
 
+      {
+        question: "Flexibilidade",
+        answer:
+          "Os contratos de estágio normalmente têm uma duração pré-definida, o que oferece à empresa maior flexibilidade ao contratar pessoas para projetos específicos ou períodos de demanda temporária.",
+        open: false,
+      },
 
+      {
+        question: "Aprendizado mútuo",
+        answer:
+          "Os estagiários trazem consigo conhecimentos e perspectivas atualizadas, tendo a oportunidade de aprender com os profissionais experientes da empresa.",
+        open: false,
+      },
+    ],
+    "Cota de Estagiário": [
+      {
+        question: "De 01 (um) a 05 (cinco) CLT ",
+        answer: "01 (um) estagiário.",
+        open: false,
+      },
+      {
+        question: "De 06 (seis) a 10 (dez) CLT  ",
+        answer: "até 02 (dois) estagiários.",
+        open: false,
+      },
+      {
+        question: "De 11 (onze) a 25 (vinte e cinco) CLT  ",
+        answer: "até 05 (cinco) estagiários.",
+        open: false,
+      },
+      {
+        question: "Acima de 25 (vinte e cinco) CLT  ",
+        answer: "até 20% (vinte por cento) de estagiários.",
+        open: false,
+      },
+    ],
+    "Benefícios Sociais": [
+      {
+        question: "Contribuição para a formação de futuros profissionais",
+        answer:
+          "Ao oferecer oportunidades de estágio, as empresas ajudam na formação de jovens talentos, preparando-os para o mercado de trabalho e contribuindo para a capacitação de futuros profissionais.",
+        open: false,
+      },
+      {
+        question: "Fortalecimento da Marca",
+        answer:
+          "Empresas que oferecem programas de estágio bem estruturados e valorizam o desenvolvimento profissional dos estagiários tendem a ganhar uma imagem positiva perante a sociedade e até mesmo potenciais clientes.",
+        open: false,
+      },
+      {
+        question: "Integração entre gerações",
+        answer:
+          "A presença de estagiários pode promover a integração entre diferentes gerações de funcionários na empresa. Os estagiários podem trazer perspectivas atualizadas, enquanto os funcionários mais experientes podem compartilhar seus conhecimentos e sabedoria.",
+        open: false,
+      },
+    ],
+    "Benefícios Trabalhistas": [
+      {
+        question: "O Caráter Formativo do Estágio",
+        answer:
+          "O estágio tem um caráter educacional e formativo, com o objetivo de proporcionar ao estudante a oportunidade de aplicar e aprimorar seus conhecimentos teóricos na prática profissional. O estagiário recebe uma bolsa-auxílio como forma de contraprestação pelo seu trabalho, que não é considerada um salário, e não estão previstos direitos trabalhistas, como FGTS, férias remuneradas ou 13º salário.",
+        open: false,
+      },
+    ],
+    "Como Aderir ao programa de estágio": [
+      {
+        question: "Termo de Convênio",
+        answer:
+          "A adesão do Programa de estágio é formalizada por meio de termo de convênio, esse termo estabelece as bases e as responsabilidades de cada parte envolvida no programa de estágio, sendo uma forma de garantir transparência e segurança nos processos.",
+        open: false,
+      },
+    ],
+    // adicionar mais se precisar
+  };
 
+  const [activeCategory, setActiveCategory] = useState<string>(categories[0]);
+  const [categoryFaqs, setCategoryFaqs] = useState(initialCategoryFaqs);
 
-  export function PrEst() {
-   
-  
-    const categories = [
-      "Benefícios Programa de Estágio",
-      "Cota de Estagiário",
-      "Benefícios Sociais",
-      "Benefícios Trabalhistas",
-      "Como Aderir ao programa de estágio"
-      // adicionar mais se precisar
-    ];
-  
-    const initialCategoryFaqs: { [key: string]: FAQItem[] } = {
-      "Benefícios Programa de Estágio": [
-        {
-          question: "Custo reduzido",
-          answer: "O estágio é regido por Legislação própria e, observados os requisitos legais, não estabelece vínculo empregatício de qualquer natureza, assim ficam isentos de alguns impostos trabalhistas, tais como: FGTS, INSS, 13º salário, ⅓ sobre férias. Isso só é possível, pois não se trata de um posto com registro na CTPS (Carteira de Trabalho e Previdência Social). Isso ajuda a reduzir os custos e possibilita as empresas investir em talentos promissores.",
-          open: false,
-        },
-        {
-          question: "Prospeção e renovação de talento",
-          answer: "Contratar estagiários permite que a empresa atraia talentos, trazendo novas ideias, perspectivas e habilidades para a sua equipe possibilitando a identificação dos mais talentosos e adequados para a cultura organizacional, permitindo que a empresa avalie potenciais futuros colaboradores.",
-          open: false,
-        },
-        {
-          question: "Treinamentos e desenvolvimentos",
-          answer: "Os estagiários são excelentes candidatos para treinamento e desenvolvimento, pois ainda estão em fase de aprendizado e podem ser moldados de acordo com as necessidades da empresa.",
-          open: false,
-        },
-        
-        {
-          question: "Flexibilidade",
-          answer: "Os contratos de estágio normalmente têm uma duração pré-definida, o que oferece à empresa maior flexibilidade ao contratar pessoas para projetos específicos ou períodos de demanda temporária.",
-          open: false,
-        },
-        
-        {
-          question: "Aprendizado mútuo",
-          answer: "Os estagiários trazem consigo conhecimentos e perspectivas atualizadas, tendo a oportunidade de aprender com os profissionais experientes da empresa.",
-          open: false,
-        },
-       
-        
-        
-      ],
-      "Cota de Estagiário": [
-        {
-          question: "De 01 (um) a 05 (cinco) CLT ",
-          answer: "01 (um) estagiário.",
-          open: false,
-        },
-        {
-          question: "De 06 (seis) a 10 (dez) CLT  ",
-          answer: "até 02 (dois) estagiários.",
-          open: false,
-        },
-        {
-          question: "De 11 (onze) a 25 (vinte e cinco) CLT  ",
-          answer: "até 05 (cinco) estagiários.",
-          open: false,
-        },
-        {
-          question: "Acima de 25 (vinte e cinco) CLT  ",
-          answer: "até 20% (vinte por cento) de estagiários.",
-          open: false,
-        },
-      ],
-      "Benefícios Sociais": [
-        {
-          question: "Contribuição para a formação de futuros profissionais",
-          answer: "Ao oferecer oportunidades de estágio, as empresas ajudam na formação de jovens talentos, preparando-os para o mercado de trabalho e contribuindo para a capacitação de futuros profissionais.",
-          open: false,
-        },
-        {
-          question: "Fortalecimento da Marca",
-          answer: "Empresas que oferecem programas de estágio bem estruturados e valorizam o desenvolvimento profissional dos estagiários tendem a ganhar uma imagem positiva perante a sociedade e até mesmo potenciais clientes.",
-          open: false,
-        },
-        {
-          question: "Integração entre gerações",
-          answer: "A presença de estagiários pode promover a integração entre diferentes gerações de funcionários na empresa. Os estagiários podem trazer perspectivas atualizadas, enquanto os funcionários mais experientes podem compartilhar seus conhecimentos e sabedoria.",
-          open: false,
-        },
-      ],
-      "Benefícios Trabalhistas": [
-        {
-          question: "O Caráter Formativo do Estágio",
-          answer: "O estágio tem um caráter educacional e formativo, com o objetivo de proporcionar ao estudante a oportunidade de aplicar e aprimorar seus conhecimentos teóricos na prática profissional. O estagiário recebe uma bolsa-auxílio como forma de contraprestação pelo seu trabalho, que não é considerada um salário, e não estão previstos direitos trabalhistas, como FGTS, férias remuneradas ou 13º salário.",
-          open: false,
-        },
-    
-      ],
-      "Como Aderir ao programa de estágio": [
-        {
-          question: "Termo de Convênio",
-          answer: "A adesão do Programa de estágio é formalizada por meio de termo de convênio, esse termo estabelece as bases e as responsabilidades de cada parte envolvida no programa de estágio, sendo uma forma de garantir transparência e segurança nos processos.",
-          open: false,
-        },
-      
-      ],
-      // adicionar mais se precisar
-    };
-  
-    const [activeCategory, setActiveCategory] = useState<string>(categories[0]);
-    const [categoryFaqs, setCategoryFaqs] = useState(initialCategoryFaqs);
-  
-    const handleCategoryChange = (category: string) => {
-      setActiveCategory(category);
-    };
-  
-    const toggleFAQ = (index: number) => {
-      setCategoryFaqs((prevCategoryFaqs) => {
-        return {
-          ...prevCategoryFaqs,
-          [activeCategory]: prevCategoryFaqs[activeCategory].map((faq, i) => {
-            if (i === index) {
-              return {
-                ...faq,
-                open: !faq.open,
-              };
-            }
+  const handleCategoryChange = (category: string) => {
+    setActiveCategory(category);
+  };
+
+  const toggleFAQ = (index: number) => {
+    setCategoryFaqs((prevCategoryFaqs) => {
+      return {
+        ...prevCategoryFaqs,
+        [activeCategory]: prevCategoryFaqs[activeCategory].map((faq, i) => {
+          if (i === index) {
             return {
               ...faq,
-              open: false,
+              open: !faq.open,
             };
-          }),
-        };
-      });
-    };
+          }
+          return {
+            ...faq,
+            open: false,
+          };
+        }),
+      };
+    });
+  };
   return (
     <>
-    <section className="ContainerProgramadeEstagio">
-      <div className="section-head prEst">
-        <h4><span>Oque é o</span>Programa de Estágio</h4>
-        <div className='divider__title'>
-            
-          <span className="hr-line"><span><span></span></span></span>
-          
+      <section className="ContainerProgramadeEstagio">
+        <div className="section-head prEst">
+          <h4>
+            <span>Oque é o</span>Programa de Estágio
+          </h4>
+          <div className="divider__title">
+            <span className="hr-line">
+              <span>
+                <span></span>
+              </span>
+            </span>
           </div>
-        <p className="gestaodh">
-        O programa de estágio é um projeto que desenvolve estudantes por meio de estágio para atuar em diversas áreas do mercado de trabalho, visa primordialmente proporcionar aprendizado assegurando que os estagiários alcancem um desempenho profisisonal notável, enquanto a empresa contrante colhe os benefícios da legislação de incentivo ao estágio sem lidar com as complicações burocráticas.
-        </p>
-      </div>
+          <p className="gestaodh">
+            O programa de estágio é um projeto que desenvolve estudantes por
+            meio de estágio para atuar em diversas áreas do mercado de trabalho,
+            visa primordialmente proporcionar aprendizado assegurando que os
+            estagiários alcancem um desempenho profissional notável, enquanto a
+            empresa contratante colhe os benefícios da legislação de incentivo
+            ao estágio sem lidar com as complicações burocráticas.
+          </p>
+        </div>
 
-      {/* =========================================================FAQ=============================================================================================================== */}
+        {/* =========================================================FAQ=============================================================================================================== */}
 
-      <div className="containerFAQ">
-        <div id="faq-links">
-          {categories.map((category, index) => (
-            <div className="faq-faq">
+        <div className="containerFAQ">
+          <div id="faq-links">
+            {categories.map((category, index) => (
+              <div className="faq-faq">
+                <div
+                  key={index}
+                  className={activeCategory === category ? "faq-selected" : ""}
+                  onClick={() => handleCategoryChange(category)}
+                >
+                  {category}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
 
-            
-            <div 
-              key={index}
-              
-              className={ activeCategory === category ? "faq-selected" : "" }
-              onClick={() => handleCategoryChange(category)}
-            >
-              {category}
-            </div>
-            </div>
+        <div className="faqs faq-grid">
+          {categoryFaqs[activeCategory].map((faq, index) => (
+            <FAQ faq={faq} index={index} key={index} toggleFAQ={toggleFAQ} />
           ))}
         </div>
-       
-      </div>
+      </section>
 
-      <div className="faqs faq-grid">
-        {categoryFaqs[activeCategory].map((faq, index) => (
-          <FAQ faq={faq} index={index} key={index} toggleFAQ={toggleFAQ} />
-        ))}
-      </div>
-    </section>
-
-  <Varejo_Farmacia/>
-  <CarouselComponent/>
-  <Testimonials_/>
+      <Varejo_Farmacia />
+      <CarouselComponent />
+      <Testimonials_ />
     </>
   );
 }
